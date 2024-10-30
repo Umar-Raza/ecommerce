9f870a15b3fd21a09619d00e388081509c26171d
Umar-Raza <chintoraza279@gmail.com>
2024-10-30 21:35:29 +0500

    login error fixed

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { MyContext } from '../../context/myContext'
 import toast from 'react-hot-toast'
 import { signInWithEmailAndPassword } from 'firebase/auth'
@@ -9,40 +9,43 @@ import { collection, onSnapshot, query, where } from 'firebase/firestore'
 export const Login = () => {
     const context = useContext(MyContext)
     const { loading, setLoading } = context;
-    const [userLogin, setUserLogin] = useState({ email: "", password: "" })
+    const navigate = useNavigate()
+    const [userLogin, setUserLogin] = useState({
+        email: "",
+        password: ""
+    });
 
-
-
-    const useLoginFuction = async () => {
+    const userLoginFunction = async () => {
         // validation 
         if (userLogin.email === "" || userLogin.password === "") {
             toast.error("All Fields are required")
         }
-
         setLoading(true);
         try {
             const users = await signInWithEmailAndPassword(auth, userLogin.email, userLogin.password);
-            // console.log(users.user)
 
+            console.log("user logedin")
+            setLoading(false)
             try {
                 const q = query(
-                    collection(firestore, "users"),
+                    collection(firestore, "user"),
                     where('uid', '==', users?.user?.uid)
                 );
                 const data = onSnapshot(q, (QuerySnapshot) => {
                     let user;
                     QuerySnapshot.forEach((doc) => user = doc.data());
-                    localStorage.setItem("user", JSON.stringify(user) )
+                    localStorage.setItem("user", JSON.stringify(user))
+                    // localStorage.setItem("user", JSON.stringify(user))
                     setUserLogin({
                         email: "",
                         password: ""
                     })
                     toast.success("Login Successfully");
                     setLoading(false);
-                    if(user.role === "user") {
-                        navigate('/user-dashboard');
-                    }else{
-                        navigate('/admin-dashboard');
+                    if (user.role === "user") {
+                        navigate('/userDashboard');
+                    } else {
+                        navigate('/adminDashboard');
                     }
                 });
                 return () => data;
@@ -50,10 +53,11 @@ export const Login = () => {
                 console.log(error);
                 setLoading(false);
             }
-        } catch (error) {
+        }
+        catch (error) {
             console.log(error);
             setLoading(false);
-            toast.error("Login Failed");
+            toast.error("Login Failed!");
         }
     }
 
@@ -72,7 +76,7 @@ export const Login = () => {
                         <form className="space-y-4 md:space-y-6" >
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-dark dark:text-primary ">Your email</label>
-                                <input type="email" value={userLogin.email} onChange={(e) => {
+                                <input type="email" name='email' value={userLogin.email} onChange={(e) => {
                                     setUserLogin({
                                         ...userLogin,
                                         email: e.target.value
@@ -81,15 +85,15 @@ export const Login = () => {
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-dark dark:text-dark ">Password</label>
-                                <input type="password" placeholder="••••••••" value={userLogin.password} onChange={(e) => {
+                                <input type="password" name='password' placeholder="••••••••" value={userLogin.password} onChange={(e) => {
                                     setUserLogin({
                                         ...userLogin,
                                         password: e.target.value
                                     })
                                 }} className="bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
-                            <button type="button" onClick={useLoginFuction} className="w-full text-white bg-dark hover:bg-primary  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-dark dark:focus:ring-dark"> {!loading ? "LOGIN" : <span className="grid w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
-                                <svg class="text-light animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
+                            <button type="button" onClick={userLoginFunction} className="w-full text-white bg-dark hover:bg-primary  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-dark dark:focus:ring-dark"> {!loading ? "LOGIN" : <span className="grid w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
+                                <svg className="text-light animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
                                     width="24" height="24">
                                     <path
                                         d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
