970c0949d19701e73f0c1bddc48fd741befa4790
Umar-Raza <chintoraza279@gmail.com>
2024-11-01 21:31:15 +0500

    get allProducts in admin dashboard

--- src/components/admin/ProductDetail.jsx
+++ src/components/admin/ProductDetail.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { MyContext } from "../../context/myContext";
 const ProductDetail = () => {
+    const context = useContext(MyContext)
+    const { loading, getAllProduct } = context
+    console.log(loading)
     return (
         <div>
             <div className="py-7 px-5 flex justify-between items-center">
@@ -12,35 +17,69 @@ const ProductDetail = () => {
                     </button>
                 </Link>
             </div>
+            {!loading ?
+                < div className="w-full overflow-x-auto px-5">
+                    <table className="w-full text-left border border-collapse sm:border-separate border-dark rounded-2xl text-dark" >
+                        <tbody>
+                            <tr>
+                                <th scope="col" className="h-12 px-6 text-md border-l first:border-l-0 border-dark text-slate-700 bg-slate-100 font-bold fontPara">S.No.</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Image</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Title</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Price</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Category</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Date</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Action</th>
+                                <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Action</th>
+                            </tr>
+                            {getAllProduct.map((item, index) => {
+                                const { id, title, price, category, date, productImgUrl } = item
+                                return <tr key={index} className="text-dark">
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 ">
+                                        {index + 1}
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                                        <img src={productImgUrl} alt="" />
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                                        {title}
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                                        Rs.{price}
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                                        {category}
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                                        {date}
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
+                                        Edit
+                                    </td>
+                                    <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
+                                        Delete
+                                    </td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                :
+                <div className="flex justify-center">
 
-            {/* table  */}
-            <div className="w-full overflow-x-auto px-5">
-                <table className="w-full text-left border border-collapse sm:border-separate border-dark rounded-2xl text-dark" >
-                    <tbody>
-                        <tr>
-                            <th scope="col" className="h-12 px-6 text-md border-l first:border-l-0 border-dark text-slate-700 bg-slate-100 font-bold fontPara">S.No.</th>
-                            <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Location Name</th>
-                            <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Action</th>
-                            <th scope="col" className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-dark text-slate-700 bg-slate-100">Action</th>
-                        </tr>
-                        <tr className="text-dark">
-                            <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 ">
-                                1.
-                            </td>
-                            <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                                {'name'}
-                            </td>
-                            <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
-                                Edit
-                            </td>
-                            <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-dark stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
-                                Delete
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                    <svg className="text-dark animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        width="40" height="40">
+                        <path
+                            d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                            stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path
+                            d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                            stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" className="text-plane">
+                        </path>
+                    </svg>
+                </div>
+
+            }
+        </div >
     );
 }
 
